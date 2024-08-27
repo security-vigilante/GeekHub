b03d5d0e2d785fc6a8b2d5a95bebe03e063bcb4e
LeeYeonJin <161997875+printjin-gmailcom@users.noreply.github.com>
2024-08-27 12:16:33 +0900

    Revert "[Feat] Floating 컴포넌트 퍼블리싱"

--- src/app/(test)/jin/page.tsx
+++ src/app/(test)/jin/page.tsx
@@ -1,17 +1,11 @@
-'use client';
+import InfoBox from '../../../components/InfoBox/InfoBox';
 
-import { FloatingButton } from '../../../components/Floating/FloatingButton';
-
-export default function HomeApp() {
+export default function App() {
   return (
-    <div>
-      <div className="h-[100rem]">아무글자</div>
-      <FloatingButton
-        type="ask"
-        onClick={() => alert('Ask Button Clicked!')}
-        className="fixed bottom-4 right-4"
-      />
-      <FloatingButton type="top" className="fixed bottom-4 left-4" />
+    <div className="p-[1rem]">
+      <InfoBox theme="red" location bullet description={[' ']} />
+      <InfoBox theme="primary" location={false} bullet description={['a']} />
+      <InfoBox theme="gray" location bullet description={['a', 'b']} />
     </div>
   );
 }
