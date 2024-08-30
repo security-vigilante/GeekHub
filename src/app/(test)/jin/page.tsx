7741ce45d95056716c8b36791aab5a6116d1b065
printjin <printjin@gmail.com>
2024-08-30 10:46:17 +0900

    Test: 테스트 페이지 수정

--- src/app/(test)/jin/page.tsx
+++ src/app/(test)/jin/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
-import { FloatingButton } from '../../../components/Floating/FloatingButton';
+import FloatingButton from '@/components/Floating/FloatingButton';
 
 export default function App() {
   return (
     <div>
       <h1>Test Page</h1>
-      <FloatingButton type="top" />
+      <FloatingButton
+        type="ask"
+        className="left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform"
+      />
     </div>
   );
 }
